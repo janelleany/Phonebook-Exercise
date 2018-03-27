85a818f8c47900b84fe92e5b858100e508731e6b
janelleany <janellealexander12@gmail.com>
2018-03-27 10:41:26 -0400

    minor setup changes

--- main.js
+++ main.js
@@ -1,54 +1,72 @@
 var fs = require("fs");
 var rl = require("readline");
+var http = require("http");
 var promisify = require("util").promisify;
 
+var routes = [
+    {}
+]
 
-var prompt = "Electronic Phone Book\
-=====================\
-1. Look up an entry\
-2. Set an entry\
-3. Delete an entry\
-4. List all entries\
-5. Save entries\
-6. Restore saved entries\
-7. Quit\
-What would you like to do (1-7)? "
-
-
-var interface = rl.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-var phonebook = [];
-
-
-interface.question(prompt, runThisCodeUsingThe(answer));
-
-
-var runThisCodeUsingThe = function(answer) {
-    
-    if (answer === )
-}
-
-
-var janellesRidiculousPromiseExercise = new Promise(function(theNativeSetStateToResolvedFunction, theNativeSetStateToRejectedFunction) {
-    var num1 = 2;
-    var num2 = 3;
-    var sum = num1 + num2;
-    if (sum === 5) {
-        theNativeSetStateToResolvedFunction("when this promise's state is set to 'resolved' by invoking the native 'Resolve' callback function, JS also created this value I can pass around/use later. Optional.");
-    } else {
-        theNativeSetStateToRejectedFunction("when this promise's state is set to 'rejected' by invoking the native 'Reject' callback function, JS also created this value I can pass around/use later. Optional. Should probably be the error object.")
+// Router
+var server = http.createServer(function(incoming, response) {
+    if (incoming.method === "GET" && incoming.url === "/create") {
+        response.
     }
+    response.end();
 })
 
-var promiseStateEqualsResolved = function(theResultValueISetInThePromise) {
-    console.log("I passed this long-ass sentence into here from the promise: " + theResultValueISetInThePromise);
+server.listen(3000);
+
+var phonebook = [
+    {
+        id: 0,
+        first: "Janelle",
+        last: "Alexander",
+        mobile: "917-721-5767",
+        email: "janelle@janelle.com"
+    },
+    {
+        id: 1,
+        first: "Greg",
+        last: "Harris",
+        mobile: "917-678-4372",
+        email: "greg@greg.com"
+    }
+];
+
+var lastID = 1;
+
+//function: LOOK UP AN ENTRY BY ID
+var lookup = function(id) {
+    phonebook.forEach(function(item, i, array) {
+            if (item.id === id) {
+                console.log("Found entry for: " + item.first + " " + item.last);
+                return true;
+            } else {
+                console.log("Entry not found");
+                return false;
+            }
+        })
 }
 
-var promiseStateEqualsRejected = function(theResultValueISetInThePromise) {
-    console.log("I passed this long-ass sentence into here from the promise: " + theResultValueISetInThePromise);
+//function: NEW ENTRY
+var add = function(first, last, mobile, email) {
+    var newID = lastID + 1; //this is horrible, janelle
+    lastID = newID; //horrible, part II
+
+    var newEntry = {
+        "id": newID,
+        "first": first,
+        "last": last,
+        "mobile": mobile,
+        "email": email
+    };
+    phonebook.push(newEntry);
+    console.log("Entry stored for " + first + " " + last);
 }
 
-janellesRidiculousPromiseExercise.then(promiseStateEqualsResolved, promiseStateEqualsRejected);
+//function: DELETE AN ENTRY
+// var del = function(firstName) {
+//     phonebook.findIndex(function(element, index, array) {
+
+//     })
